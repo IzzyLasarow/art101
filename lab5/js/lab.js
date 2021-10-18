b20432158cf3faeecc37932a5daf3c51f8d0fd40
IzzyLasarow <91556635+IzzyLasarow@users.noreply.github.com>
2021-10-18 15:46:36 -0700

    lab 5
    changes

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+  * Author: Izzy Lasarow <ilasarow@ucsc.edu>
+  * Created: 18 October
+  * License: Public Domain
+*/
+
+// Define Variables
+make = "Nissan";
+model = "Altima";
+color = "Rodeo Red";
+year = 2008;
+
+//calculate
+age = 2021 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
